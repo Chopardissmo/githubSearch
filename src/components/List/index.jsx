fd9b1064fa8f69e16bd0abfafb9caa9da4b950c9
Chopardissmo <gel@alumni.usc.edu>
2021-05-25 03:08:58 +0800

    change the view for List component based on state

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,78 +3,35 @@ import "./index.css";
 
 export default class List extends Component {
     render() {
+        const { users, isFirst, isLoading, err } = this.props;
         return (
             <div className="row">
-                <div className="card">
-                    <a
-                        rel="noreferrer"
-                        href="https://github.com/reactjs"
-                        target="_blank"
-                    >
-                        <img
-                            alt="avatar"
-                            src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                            style={{ width: "100px" }}
-                        />
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a
-                        rel="noreferrer"
-                        href="https://github.com/reactjs"
-                        target="_blank"
-                    >
-                        <img
-                            alt="avatar"
-                            src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                            style={{ width: "100px" }}
-                        />
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a
-                        rel="noreferrer"
-                        href="https://github.com/reactjs"
-                        target="_blank"
-                    >
-                        <img
-                            alt="avatar"
-                            src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                            style={{ width: "100px" }}
-                        />
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a
-                        rel="noreferrer"
-                        href="https://github.com/reactjs"
-                        target="_blank"
-                    >
-                        <img
-                            alt="avatar"
-                            src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                            style={{ width: "100px" }}
-                        />
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
-                <div className="card">
-                    <a
-                        rel="noreferrer"
-                        href="https://github.com/reactjs"
-                        target="_blank"
-                    >
-                        <img
-                            alt="avatar"
-                            src="https://avatars.githubusercontent.com/u/6412038?v=3"
-                            style={{ width: "100px" }}
-                        />
-                    </a>
-                    <p className="card-text">reactjs</p>
-                </div>
+                {isFirst ? (
+                    <h1>Enter UserName to Search</h1>
+                ) : isLoading ? (
+                    <h2>Loading........</h2>
+                ) : err ? (
+                    <h2>{err}</h2>
+                ) : (
+                    users.map((user) => {
+                        return (
+                            <div className="card" key={user.id}>
+                                <a
+                                    rel="noreferrer"
+                                    href={user.html_url}
+                                    target="_blank"
+                                >
+                                    <img
+                                        alt="avatar"
+                                        src={user.avatar_url}
+                                        style={{ width: "100px" }}
+                                    />
+                                </a>
+                                <p className="card-text">{user.login}</p>
+                            </div>
+                        );
+                    })
+                )}
             </div>
         );
     }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -3,14 +3,37 @@ import Axios from "axios";
 
 export default class Search extends Component {
     search = () => {
+        const { updateAppState } = this.props;
         //获取用户输入
         const { value } = this.keywordElement;
-        console.log(value);
+
+        updateAppState({ isFirst: false, isLoading: true });
+
         //发送AJAX请求
         // url 可以简写`/api/search/users?q=${value}`
         Axios.get(`http://localhost:3000/api/search/users?q=${value}`).then(
-            (response) => console.log("success", response.data),
-            (error) => console.log("fail", error)
+            (response) => {
+                console.log("success", response.data);
+                const users = response.data.items.map((item) => {
+                    const user = {};
+                    user.id = item.id;
+                    user.login = item.login;
+                    user.avatar_url = item.avatar_url;
+                    user.html_url = item.html_url;
+                    return user;
+                });
+                updateAppState({
+                    isLoading: false,
+                    users: users,
+                    err: null,
+                });
+            },
+            (error) => {
+                updateAppState({
+                    isLoading: false,
+                    err: `Request Err: ${error}`,
+                });
+            }
         );
     };
     render() {
